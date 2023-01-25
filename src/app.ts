30734c51e7fd0368b7c103088fdc6a2749263412
Asnur Ramdani <asnurramdhani12@gmail.com>
2023-01-25 15:29:09 +0700

    add integration chatGPT

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,18 @@
-import { Client } from "whatsapp-web.js";
+import { Client, LocalAuth } from "whatsapp-web.js";
 import qrcode from "qrcode-terminal";
+import axios from "axios";
 
 const client = new Client({
   puppeteer: {
     args: ["--no-sandbox"],
   },
+  authStrategy: new LocalAuth({
+    clientId: "BEM-STTNF",
+    dataPath: __dirname + "/session",
+  }),
 });
+
+const { API_KEY } = require("dotenv").config().parsed;
 
 client.on("qr", (qr) => {
   qrcode.generate(qr, {
@@ -18,9 +25,24 @@ client.on("ready", () => {
 });
 
 client.on("message", (msg) => {
-  if (msg.body == "!ping") {
-    msg.reply("pong");
-  }
+  axios
+    .post(
+      "https://api.openai.com/v1/engines/davinci-codex/completions",
+      {
+        prompt: msg.body,
+        temperature: 1,
+        max_tokens: 100,
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${API_KEY}`,
+        },
+      }
+    )
+    .then((res) => {
+      msg.reply(res.data.choices[0].text);
+    });
 });
 
 client.on("disconnected", (reason) => {
@@ -28,7 +50,7 @@ client.on("disconnected", (reason) => {
 });
 
 client.on("authenticated", (session) => {
-  console.log("Client authenticated");
+  console.log("Client was authenticated");
 });
 
 client.initialize();
